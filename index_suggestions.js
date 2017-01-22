6a47080dbecb335ef0b28c5378e7e2f3d62dbc64
Rictus <contact.dylanleroux@gmail.com>
2017-01-23 00:43:05 +0100

    Resume feature

--- index_suggestions.js
+++ index_suggestions.js
@@ -2,13 +2,7 @@ var googleSuggestQuerier = require('./src/googleSuggestQuerier.js')(__dirname + 
 console.log(new Date());
 
 console.time("googleSuggestQuerier");
-
-googleSuggestQuerier.launchSimple(1, function () {
+googleSuggestQuerier.resume(1, function () {
     console.timeEnd("googleSuggestQuerier");
 });
-//*/
-/*
- googleSuggestQuerier.launchSync();
- console.timeEnd("googleSuggestQuerier");
- //*/
 
