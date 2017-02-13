b1593abca7b4902b5d52f408f76bb532984a6906
Rictus <contact.dylanleroux@gmail.com>
2017-02-13 21:06:58 +0100

    pipable

--- color_queried.sh
+++ color_queried.sh
@@ -2,26 +2,32 @@
 
 TEST_LINE="llc > fregre,gr---grth,fsd"
 
-KEYWORD=`echo $TEST_LINE | cut -d" " -f1`
-CHEVRON=`echo $TEST_LINE | cut -d" " -f2`
-SUGGESTIONS=`echo $TEST_LINE | cut -d" " -f3`
+colorize() {
+
+    LINE=`cat` # read from stdin
+    KEYWORD=`echo $LINE | cut -d" " -f1`
+    CHEVRON=`echo $LINE | cut -d" " -f2`
+    SUGGESTIONS_LINE=`echo $LINE | cut -d" " -f3`
 
 
-echo -en "$(tput bold)$(tput setaf 6)$KEYWORD$(tput sgr0)""$(tput setaf 3) $CHEVRON $(tput sgr0)"
+    echo -en "$(tput bold)$(tput setaf 6)$KEYWORD$(tput sgr0)""$(tput setaf 3) $CHEVRON $(tput sgr0)"
 
 
-OLD_IFS=$IFS
-IFS=',' read -ra SUGGESTIONS <<< "$SUGGESTIONS"
-for SUGGESTION in "${SUGGESTIONS[@]}"; do
-    SUGGESTION_GREP=`echo $SUGGESTION|grep --color=always -e "[^a-zA-Z0-9]"`
+    OLD_IFS=$IFS
+    IFS=',' read -ra SUGGESTIONS <<< "$SUGGESTIONS_LINE"
+    for SUGGESTION in "${SUGGESTIONS[@]}"; do
+        SUGGESTION_GREP=`echo $SUGGESTION|grep --color=always -e "[^a-zA-Z0-9]"`
 
-    if [ ${#SUGGESTION_GREP} -eq 0 ];
-    then
-        SUGGESTION_GREP=$SUGGESTION
-    fi
+        if [ ${#SUGGESTION_GREP} -eq 0 ];
+        then
+            SUGGESTION_GREP=$SUGGESTION
+        fi
 
-    echo -en "$(tput setaf 7)$SUGGESTION_GREP$(tput sgr 0)$(tput setaf 2),$(tput sgr 0) "
-done
-IFS=$OLD_IFS
+        echo -en "$(tput setaf 7)$SUGGESTION_GREP$(tput sgr 0)$(tput setaf 2),$(tput sgr 0) "
+    done
+    IFS=$OLD_IFS
 
-tput sgr0
+    tput sgr0
+}
+
+tail -f $1|colorize
